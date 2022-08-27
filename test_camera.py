e2e4f7446e94a9864c90cd27a981d2d6264a0218
mobone <nicholas.reichel@gmail.com>
2022-08-27 14:08:47 -0500

    added focuser

--- test_camera.py
+++ test_camera.py
@@ -1,3 +1,9 @@
 from RpiCamera import Camera
+import cv2
+from Focuser import Focuser
+
+
+focuser = Focuser(10)
+
 camera = Camera()
-cv2.imwrite("current_card.jpg"), camera.getFrame()
+cv2.imwrite("current_card.jpg", camera.getFrame())
