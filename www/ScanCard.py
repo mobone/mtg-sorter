2e9ac0375428894c9985cb123af81a327841ed04
mobone <nicholas.reichel@gmail.com>
2022-08-27 15:33:04 -0500

    change dir

--- www/ScanCard.py
+++ www/ScanCard.py
@@ -18,22 +18,22 @@ signal.signal(signal.SIGTERM, sigint_handler)
 def parse_cmdline():
     parser = argparse.ArgumentParser(description='Arducam IMX519 Autofocus Demo.')
 
-    parser.add_argument('-i', '--i2c-bus', type=int, nargs=None, required=True,
+    parser.add_argument('-i', '--i2c-bus', type=int, nargs=None, required=False,
                         help='Set i2c bus, for A02 is 6, for B01 is 7 or 8, for Jetson Xavier NX it is 9 and 10.')
 
     parser.add_argument('-v', '--verbose', action="store_true", help='Print debug info.')
 
     return parser.parse_args()
 
-if __name__ == "__main__":
-    args = parse_cmdline()
+def scan_card():
+    #args = parse_cmdline()
     camera = Camera()
     camera.start_preview(False)
-    focuser = Focuser(args.i2c_bus)
-    focuser.verbose = args.verbose
+    focuser = Focuser(10)
+    #focuser.verbose = args.verbose
 
     focusState = FocusState()
-    focusState.verbose = args.verbose
+    #focusState.verbose = args.verbose
     doFocus(camera, focuser, focusState)
 
     start = time.time()
