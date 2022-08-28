57e5caee47d99eec6f16a02b37320cb826d1f586
mobone <nicholas.reichel@gmail.com>
2022-08-28 18:07:50 -0500

    Update magic_card_detector.py

--- www/magic_card_detector.py
+++ www/magic_card_detector.py
@@ -632,8 +632,8 @@ class MagicCardDetector:
         with open(path, 'rb') as filename:
             hashed_list = pickle.load(filename)
         for ref_im in hashed_list:
-            if 'tusk' not in ref_im.name.lower():
-                continue
+            #if 'tusk' not in ref_im.name.lower():
+                #continue
             self.reference_images.append(
                 ReferenceImage(ref_im.name, None, self.clahe, ref_im.phash))
         print('Done.')
@@ -865,6 +865,13 @@ class MagicCardDetector:
         #rotations = np.array([0., 90., 180., 270.])
         rotations = np.array([0.])
         d_0_dist = np.zeros(len(rotations))
+        this_reference_images = []
+        for img in self.reference_images:
+            if 'tusk' in img.name:
+                this_reference_images.append(img)
+
+        self.reference_images = this_reference_images
+            
         d_0 = np.zeros((len(self.reference_images), len(rotations)))
         for j, rot in enumerate(rotations):
             if not -1.e-5 < rot < 1.e-5:
