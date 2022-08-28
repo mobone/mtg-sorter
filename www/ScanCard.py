8db90767f661c28eb11bd6a418cbef799b494ad0
mobone <nicholas.reichel@gmail.com>
2022-08-28 17:04:57 -0500

    test

--- www/ScanCard.py
+++ www/ScanCard.py
@@ -46,10 +46,10 @@ def scan_card():
 
         if focusState.isFinish():
             exit_ = True
-    time.sleep(.1)
+    #time.sleep(.1)
     frame = camera.getFrame()
     img = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    #img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
     cv2.imwrite("./static/current_scan.jpg", img)
     print('wrote file')
