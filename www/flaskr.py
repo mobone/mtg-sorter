4723a76d7cf440694a03d8fb054a2f65887e8273
mobone <nicholas.reichel@gmail.com>
2022-08-30 16:50:01 -0500

    Update flaskr.py

--- www/flaskr.py
+++ www/flaskr.py
@@ -27,6 +27,17 @@ recognized_cards = []
 
 magic_card_detector.load()
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 def get_text():
     img = cv2.imread('./static/current_scan.jpg')
