de3db5cf3ed6b6f11156927d0211e22ed4e5ba27
mobone <nicholas.reichel@gmail.com>
2022-08-30 16:29:27 -0500

    Update flaskr.py

--- www/flaskr.py
+++ www/flaskr.py
@@ -37,24 +37,24 @@ def get_text():
 
     cv2.imwrite('/home/admn/Documents/mtg-sorter/cropped.jpg', img)
     
-    #scale_percent = 50 # percent of original size
-    #width = int(img.shape[1] * scale_percent / 100)
-    #height = int(img.shape[0] * scale_percent / 100)
-    #dim = (width, height)
+    scale_percent = 50 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
 
-    #img_scaled = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    img_scaled = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
     gray_img = get_grayscale(img)
-    #opening_img = opening(gray_img)
+    opening_img = opening(gray_img)
 
-    #gray_img_scaled = get_grayscale(img_scaled)
-    #opening_img_scaled = opening(gray_img_scaled)
+    gray_img_scaled = get_grayscale(img_scaled)
+    opening_img_scaled = opening(gray_img_scaled)
     
     custom_config = r'--oem 3 --psm 4'
     
     text_gray_img = pytesseract.image_to_string(gray_img, config=custom_config)
-    #text_original_scaled = pytesseract.image_to_string(opening_img_scaled, config=custom_config)
-    #text_gray_img_scaled = pytesseract.image_to_string(gray_img_scaled, config=custom_config)
+    text_original_scaled = pytesseract.image_to_string(opening_img_scaled, config=custom_config)
+    text_gray_img_scaled = pytesseract.image_to_string(gray_img_scaled, config=custom_config)
 
     
     max_score = 0
@@ -62,8 +62,8 @@ def get_text():
     best_text_type = None
     best_text = None
     
-    #for text, text_type in [(text_original_scaled, 'original_scaled'),(text_gray_img, 'gray'), (text_gray_img_scaled, 'gray_scaled')]:
-    for text, text_type in [(text_gray_img, 'gray')]:
+    for text, text_type in [(text_original_scaled, 'original_scaled'),(text_gray_img, 'gray'), (text_gray_img_scaled, 'gray_scaled')]:
+    #for text, text_type in [(text_gray_img, 'gray')]:
         print(text)
         if text is None or text == '':
             continue
